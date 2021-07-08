9a8eb3c5ce484698efa69d61ff773a04471b7d61
junseo <ysj6844@naver.com>
2021-07-08 21:41:20 +0900

    first init

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            너 자신을 알라
+        </div>
+    );
+};
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,100 @@
+import path from 'path';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import webpack from 'webpack';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const config: webpack.Configuration = {
+  name: 'ala',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: !isDevelopment ? 'hidden-source-map' : 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@components': path.resolve(__dirname, 'components'),
+      '@containers': path.resolve(__dirname, 'containers'),
+      '@modules': path.resolve(__dirname, 'modules'),
+      '@pages': path.resolve(__dirname, 'pages'),
+      '@lib': path.resolve(__dirname, 'lib'),
+      '@typings': path.resolve(__dirname, 'typings'),
+      '@router': path.resolve(__dirname, 'router'),
+      '@assets': path.resolve(__dirname, 'assets'),
+    },
+  },
+  entry: {
+    app: './index',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: { browsers: ['last 2 chrome versions'] },
+                debug: isDevelopment,
+              },
+            ],
+            '@babel/preset-react',
+            '@babel/preset-typescript',
+          ],
+          env: {
+            development: {
+              plugins: [require.resolve('react-refresh/babel')],
+            },
+            // production: {
+            //   plugins: ['@emotion'],
+            // },
+          },
+        },
+        exclude: path.join(__dirname, 'node_modules'),
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      // eslint: {
+      //   files: "./src/**/*",
+      // },
+    }),
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js', // name -> entry에서 설정한 이름
+    publicPath: '/dist/',
+  },
+  devServer: {
+    historyApiFallback: true, // react router
+    port: 3000,
+    publicPath: '/dist/',
+    // proxy: {
+    //   '/api/': {
+    //     target: 'back api',
+    //     changeOrigin: true,
+    //   },
+    // },
+  },
+};
+
+if (isDevelopment && config.plugins) {
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(new ReactRefreshWebpackPlugin());
+  //   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
+}
+if (!isDevelopment && config.plugins) {
+  //   config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+  //   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+}
+
+export default config;
